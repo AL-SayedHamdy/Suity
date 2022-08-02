1317ca1b4e35f2dc2c467c11ae3e1695769c6727
AL-SayedHamdy <alsayedhamdy5566@gmail.com>
2022-08-02 10:03:46 +0200

    Some editing

---
+++ Clothes object classifier/Men's object classifier/Men object classifier.py
@@ -0,0 +1,160 @@
+##Libraries
+import os,cv2
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.preprocessing.image import ImageDataGenerator
+
+#Importing the data
+data_dir = os.path.join('F:/Work/Projects/Suity/Clothes object classifier/Data/Men')
+
+categories = ['Foot wear', 'Jacket', 'Shirt', 'Shorts', 'Sweatshirt', 'Trousers', 'T-Shirt']
+
+#Show with gray scale
+for category in categories:
+    path = os.path.join(data_dir, category)
+    for img in os.listdir(path):
+        img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+        plt.imshow(img_arr, cmap = 'gray')
+        plt.show()
+        break
+    break
+
+batch_size = 32
+img_size = 50
+epochs = 17
+
+#Show after the resize
+new_arr = cv2.resize(img_arr, (img_size, img_size))
+plt.imshow(new_arr, cmap = 'gray')
+plt.show()
+
+#Creat training data
+training_data = []
+def creat_traingin_data():
+    for category in categories:
+        path = os.path.join(data_dir, category)
+        class_num = categories.index(category)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                new_arr = cv2.resize(img_arr, (img_size, img_size))
+                training_data.append([new_arr, class_num])
+            except Exception as e:
+                pass
+creat_traingin_data()
+
+#Suffle
+random.shuffle(training_data)
+
+#Test the shuffle
+for sample in training_data[:20]:
+    print(sample[1])
+
+
+#Features and lables split
+X = []
+y = []
+
+for features, labels in training_data:
+    X.append(features)
+    y.append(labels)
+
+X = np.array(X).reshape(-1, img_size, img_size, 1)
+y = np.array(y)
+
+#Normalize the data
+X = X/255.0
+
+#Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=0)
+
+#Data augmentation
+datagen = ImageDataGenerator(rotation_range=90,
+                             width_shift_range=0.2,
+                             height_shift_range=0.2,
+                             vertical_flip=True,
+                             horizontal_flip=True)
+datagen.fit(X_train)
+
+#CNN model building
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(img_size, img_size, 1)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (4, 4), activation='relu'))
+model.add(Dropout(0.4))
+
+model.add(Flatten())
+
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.3))
+
+model.add(Dense(7, activation='softmax'))
+    
+#Compile
+model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+#Check point and early stopping
+checkpoint = ModelCheckpoint("men_classifier_up.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+
+early = EarlyStopping(monitor='val_acc', min_delta=0, patience=20, verbose=1, mode='auto')
+
+#Fitting the model
+history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test))
+
+#Visualisation
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper right')
+plt.show()
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(['Train', 'Validation'])
+plt.show()
+
+'''
+#Model save
+model.save('men_classifier_up.h5')
+'''
+
+'''
+#Model load
+model = keras.models.load_model('F:/Work/Projects/Suity/Clothes object classifier/Men classifier/men_classifier_up.h5')
+'''
+
+#Test the model
+y_pred = model.predict(X_test)
+
+y_classes = [np.argmax(element) for element in y_pred]
+
+#The accuracy and loss
+test_loss, test_acc = model.evaluate(X_test,  y_test, verbose=2)
+
+score, acc = model.evaluate(X_test, y_test, batch_size=(65))
+
+#Show sample
+def plot_sample(x, y, index):
+    plt.figure(figsize = (15,2))
+    plt.imshow(x[index], cmap = 'gray')
+    plt.xlabel(categories[y[index]])
+
+plot_sample(X_test, y_test,56)
---
+++ Clothes object classifier/Women's object classifer/App.py
@@ -0,0 +1,87 @@
+# Imports
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import uvicorn
+from PIL import Image
+from pydantic import BaseModel
+from tensorflow.keras.models import load_model
+from typing import List
+import io
+import numpy as np
+import sys
+
+# Load the model
+filepath = 'F:/Work/Projects/Suity/Clothes object classifier/Women classifer/women_classifier_up.h5'
+model = load_model(filepath, compile = True)
+
+categories = ['Dress', 'Foot wear', 'Hijab', 'Jacket', 'Jumpsuit', 'Shorts', 'Skirt', 'Sweatshirt', 'Top', 'Trousers']
+
+# Get the input shape for the model layer
+input_shape = model.layers[0].input_shape
+
+# Define the FastAPI app
+app = FastAPI()
+
+# Define the Response
+class Prediction(BaseModel):
+  filename: str
+  contenttype: str
+  prediction: List[float] = []
+  category: str
+
+# Define the main route
+@app.get('/')
+def root_route():
+  return { 'error': 'Use GET /prediction instead of the root route!' }
+
+# Define the /prediction route
+@app.post('/prediction/', response_model=Prediction)
+async def prediction_route(file: UploadFile = File(...)):
+
+# Ensure that this is an image
+  if file.content_type.startswith('image/') is False:
+    raise HTTPException(status_code=400, detail=f'File \'{file.filename}\' is not an image.')
+
+  try:
+    # Read image contents
+    contents = await file.read()
+    pil_image = Image.open(io.BytesIO(contents))
+
+    # Resize image to expected input shape
+    pil_image = pil_image.resize((input_shape[1], input_shape[2]))
+
+    # Convert from RGBA to RGB *to avoid alpha channels*
+    if pil_image.mode == 'RGBA':
+      pil_image = pil_image.convert('RGB')
+
+    # Convert image into grayscale *if expected*
+    if input_shape[3] and input_shape[3] == 1:
+      pil_image = pil_image.convert('L')
+
+    # Convert image into numpy format
+    numpy_image = np.array(pil_image).reshape((input_shape[1], input_shape[2], input_shape[3]))
+
+    # Scale data (depending on your model)
+    numpy_image = numpy_image / 255
+
+    # Generate prediction
+    prediction_array = np.array([numpy_image])
+    predictions = model.predict(prediction_array)
+    prediction = predictions[0]
+    likely_class = np.argmax(prediction)
+    category = categories[likely_class]
+
+    return {
+      'filename': file.filename,
+      'contenttype': file.content_type,
+      'prediction': prediction.tolist(),
+      'category': category
+    }
+  except:
+    e = sys.exc_info()[1]
+    raise HTTPException(status_code=500, detail=str(e))
+    
+import nest_asyncio
+nest_asyncio.apply()
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
